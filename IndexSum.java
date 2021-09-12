dc3408695da3cf2a92b7a6518e012efb806dd140
Bhoomika-Nadig <89984638+Bhoomika-Nadig@users.noreply.github.com>
2021-09-12 12:46:18 +0530

    Add files via upload

---
+++ IndexSum.java
@@ -0,0 +1,16 @@
+package com.x_workz.controlStatements;
+
+public class IndexSum {
+
+	public static void main(String[] args) {
+		int arr[] = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+		for (int i = 0; i <= 10; i++)
+			if (arr[i] % 1 == 0)
+
+			{
+				System.out.println("Array integers are = " + arr[i]);
+
+			}
+	}
+
+}
